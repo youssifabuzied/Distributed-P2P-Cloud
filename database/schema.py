25f9a7bbc82cdd419dd2c91879ff47caf8866073
omar-bahgat <omar_bahgat@aucegypt.edu>
2025-11-23 17:23:17 +0200

    Add ImageAccess table

--- database/schema.py
+++ database/schema.py
@@ -14,9 +14,27 @@ class Schema:
     def Image():
         return """
        CREATE TABLE IF NOT EXISTS Image (
-           image_name TEXT PRIMARY KEY NOT NULL,
+           image_name TEXT  NOT NULL,
            image_bytes BLOB NOT NULL,
            user_name TEXT NOT NULL,
+           PRIMARY KEY (image_name, user_name),
            FOREIGN KEY(user_name) REFERENCES Client(user_name)
        );
        """
+    
+    @staticmethod
+    def ImageAccess():
+        return '''
+       CREATE TABLE IF NOT EXISTS ImageAccess (
+            status INTEGER NOT NULL,
+            owner TEXT NOT NULL,
+            viewer TEXT NOT NULL,
+            image_name TEXT NOT NULL,
+            prop_views INTEGER NOT NULL,
+            accep_views INTEGER NOT NULL,
+            PRIMARY KEY (owner, image_name, viewer),
+            FOREIGN KEY(owner) REFERENCES Client(user_name),
+            FOREIGN KEY(viewer) REFERENCES Client(user_name),
+            FOREIGN KEY(image_name) REFERENCES Image(image_name)
+        );
+        '''
